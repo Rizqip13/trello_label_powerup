300838e615fd5410199855fb930b040fbe8668f7
Rizqi PM <rizqipm96@gmail.com>
2022-03-27 22:48:00 +0700

    add endpoint

--- powerup/__init__.py
+++ powerup/__init__.py
@@ -32,18 +32,18 @@ def create_app():
         db_session.remove()
 
 
-    @app.route("/ping/")
+    @app.route("/powerup/ping/")
     def pong():
         return {"status": "success",
               "data": {
                   "message": "pong"
               }}, 200
 
-    @app.get("/webhook/")
+    @app.get("/powerup/webhook/")
     def webhook_head():
         return Response(status=200)
 
-    @app.post("/webhook/")
+    @app.post("/powerup/webhook/")
     def webhook_post():
         action_payload = request.get_json().get("action")
         action_type    = action_payload.get("type")
@@ -62,7 +62,7 @@ def create_app():
         db_session.commit()
         return Response(status=200)
 
-    @app.get("/board/")
+    @app.get("/powerup/board/")
     def get_all_board():
         board_data, status_code = get_member_boards(KEY=KEY, TOKEN=TOKEN)
         if status_code == 200:
@@ -72,7 +72,7 @@ def create_app():
             return {"status": "Fail",
                     "data" : None}, status_code
 
-    @app.get("/board/<board_id>")
+    @app.get("/powerup/board/<board_id>")
     def get_board(board_id):
         board_data = BoardWebhookModel.query.filter(BoardWebhookModel.id == board_id).first()
         if board_data:
@@ -88,7 +88,7 @@ def create_app():
             return {"status": "Fail",
                     "data" : None}, status_code
 
-    @app.post("/board/<board_id>")
+    @app.post("/powerup/board/<board_id>")
     def post_board_webhook(board_id):
         board_data = BoardWebhookModel.query.filter(BoardWebhookModel.id == board_id).first()
         if board_data:
